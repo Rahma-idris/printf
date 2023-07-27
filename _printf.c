3aed76e2ed5e5d3e5ed89b310cf9ee8f42d96ee8
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-07-27 05:51:41 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 {
 	unsigned int i = 0, len = 0, ibuf = 0;
 	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
+	int (*function)(va_list, char *, unsigned int); //This function is used to handle format specifiers
 	char *buffer;
 
 	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);

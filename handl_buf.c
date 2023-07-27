e39c539120dd32d1d771ee0230b4979faf8ab74d
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-07-27 05:42:01 +0100

    Update handl_buf.c
    An edit to help me  understand luis chaparroc

--- handl_buf.c
+++ handl_buf.c
@@ -11,9 +11,11 @@ unsigned int handl_buf(char *buf, char c, unsigned int ibuf)
 {
 	if (ibuf == 1024)
 	{
+		//When buffer is full, print_buf the storage and then initialize it to 0. 
 		print_buf(buf, ibuf);
 		ibuf = 0;
 	}
+	//Stores buffer increment in c
 	buf[ibuf] = c;
 	ibuf++;
 	return (ibuf);
